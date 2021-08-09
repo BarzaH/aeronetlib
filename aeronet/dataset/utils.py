07a2c7e771b555c8d26b83b856c114de261775df
Mergaliev Ibragim <mer_eng@mail.ru>
2021-08-09 17:59:59 +0300

    Update utils.py
    Added windows support

--- aeronet/dataset/utils.py
+++ aeronet/dataset/utils.py
@@ -11,7 +11,6 @@ def parse_directory(directory, names, extensions=('tif', 'tiff', 'TIF', 'TIFF'))
         directory: str
         names: tuple of str, band or file names, e.g. ['RED', '101']
         extensions: tuple of str, allowable file extensions
-
     Returns:
         list of matched paths
     """
@@ -20,16 +19,13 @@ def parse_directory(directory, names, extensions=('tif', 'tiff', 'TIF', 'TIFF'))
     res = []
     for name in names:
         # the channel name must be either full filename (that is, ./RED.tif) or a part after '_' (./dse_channel_RED.tif)
-        sep = os.sep if os.sep != '\\' else '\\\\'
-        pattern = '.*(^|{}|_)({})\.({})$'.format(sep, name, extensions)
+        sep = os.sep if not os.name == 'nt' else os.sep+os.sep
+        pattern = '.*({}|_)({})\.({})$'.format(sep, name, extensions)
         band_path = [path for path in paths if re.match(pattern, path) is not None]
-
         # Normally with our datasets it will never be the case, and may indicate wrong file naming
         if len(band_path) > 1:
             warn(RuntimeWarning(
                 "There are multiple files matching the channel {}. "
                 "It can cause ambiguous behavior later.".format(name)))
         res += band_path
-
     return res
-
